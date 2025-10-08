1cc20eeef129e74391605775d3b2184563d2f73c
Maksym Derezyuk <100006117.joan23@fje.edu>
2025-10-08 19:45:20 +0200

    Index.php con formulario
    Creación de index.php con el formulario de las figuras

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Figuras Geométricas</title>
+</head>
+<body>
+    <h1>Seleccione una figura geométrica</h1>
+    <form action=" " method="post">
+        <label>
+            <input type="radio" name="figura" value="triangulo">Triángulo
+        </label><br>
+        <label>
+            <input type="radio" name="figura" value="rectangulo">Rectángulo
+        </label><br>
+        <label>
+            <input type="radio" name="figura" value="cuadrado">Cuadrado
+        </label><br>
+        <label>
+            <input type="radio" name="figura" value="circulo">Círculo
+        </label><br><br>
+        <button type="submit">Continuar</button>
+    </form>
+</body>
+</html>
